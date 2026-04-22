bee47a0e85d0f5a8788305f900c403a4f473fc0c
Miguel Angel Moya <miguel@moleaer.com>
2026-04-22 23:41:33 +0200

    se añade appserviceprovider para el formulario https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     {{-- CARGA DE CSS Y JS CON VITE --}}
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    
+    @vite(['resources/css/app.css', 'resources/js/app.js']) 
 </head>
 
 <body>
